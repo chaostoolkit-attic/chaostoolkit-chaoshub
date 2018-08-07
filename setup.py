e7df79fe7da797b9a4750492f7ef04dc27869cc9
Sylvain Hellegouarch <sh@defuze.org>
2018-08-07 08:28:39 +0200

    Read version without importing
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- setup.py
+++ setup.py
@@ -3,12 +3,22 @@
 
 import sys
 import io
+import os
 
 import setuptools
 
-sys.path.insert(0, ".")
-from chaoshub import __version__
-sys.path.remove(".")
+def get_version_from_package() -> str:
+    """
+    Read the package version from the source without importing it.
+    """
+    path = os.path.join(os.path.dirname(__file__), "chaoshub/__init__.py")
+    path = os.path.normpath(os.path.abspath(path))
+    with open(path) as f:
+        for line in f:
+            if line.startswith("__version__"):
+                token, version = line.split(" = ", 1)
+                version = version.replace("'", "").strip()
+                return version
 
 name = 'chaostoolkit-chaoshub'
 desc = 'Chaos Hub plugin for the Chaos Toolkit project'
@@ -49,7 +59,7 @@ with io.open('requirements.txt') as f:
 
 setup_params = dict(
     name=name,
-    version=__version__,
+    version=get_version_from_package(),
     description=desc,
     long_description=long_desc,
     classifiers=classifiers,
